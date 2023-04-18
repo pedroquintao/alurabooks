4c8be5e7659fb2ca569a2cc9a981d4c02caa7330
pedroquintao <pedro.h.quintao@hotmail.com>
2023-04-18 12:24:24 -0300

    Aula 4 - Crian... - Promise All

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try{
-        var consultaCEP = await fetch('https://viacep.com.br/ws/38182348/json/');
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultaCEP.json();
         if(consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
         }
+
+        // console.log('%cscript.js line:9 consultaCEPConvertida', 'color: #007acc;', consultaCEPConvertida);
+        return consultaCEPConvertida;
     }
 
     catch(erro) {
-        console.log('%cscript.js line:8 erro', 'color: #007acc;', erro);
+        // console.log('%cscript.js line:8 erro', 'color: #007acc;', erro);
     }
-    
 
-    console.log('%cscript.js line:3 consultaCEP', 'color: #007acc;', consultaCEPConvertida);
+    // console.log('%cscript.js line:3 consultaCEP', 'color: #007acc;', consultaCEPConvertida);
 }
 
-buscaEndereco();
+let ceps = [38182348, 35900166, 35904010]
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+console.log('%cscript.js line:22 Antes do Promise', 'color: #007acc;', conjuntoCeps);
+Promise.all(conjuntoCeps).then(respostas => console.log('%cscript.js line:23 Depois do Promise:', 'color: #007acc;', respostas));
+// buscaEndereco();
 // var consultaCEP = fetch('https://viacep.com.br/ws/0100100/json/') //O fetch devolve uma promessa de que algo será retornado, essa promessa é chamada de Promisse. Essa promessa pode tanto ser boa, ter retornado os dados, quanto ter falhado por algum motivo - como no caso da conexão com o servidor cair. 
 // .then(resposta => resposta.json())
 // .then(r => {
